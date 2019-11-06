a1d9a8e64a91e9d67d97a9d85087b0b3aac6b1c2
thiagovasconcellos <thiagovasconcellos88@gmail.com>
2019-11-06 19:59:56 -0300

    added styles to pages

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://img.terabyteshop.com.br/produto/g/monitor-gamer-hq-curvo-27-pol-165hz-1ms-freesync-hdmi-display-port_82529.jpg"
+                alt=""
+                width={128}
+                height={128}
+              />
+            </td>
+            <td>
+              <strong>Pqp que merda</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#715c91" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#715c91" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,80</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://img.terabyteshop.com.br/produto/g/monitor-gamer-hq-curvo-27-pol-165hz-1ms-freesync-hdmi-display-port_82529.jpg"
+          alt="Monitor gamer"
+        />
+        <strong>Monitor gamer</strong>
+        <span>R$1.729,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://img.terabyteshop.com.br/produto/g/monitor-gamer-hq-curvo-27-pol-165hz-1ms-freesync-hdmi-display-port_82529.jpg"
+          alt="Monitor gamer"
+        />
+        <strong>Monitor gamer</strong>
+        <span>R$1.729,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://img.terabyteshop.com.br/produto/g/monitor-gamer-hq-curvo-27-pol-165hz-1ms-freesync-hdmi-display-port_82529.jpg"
+          alt="Monitor gamer"
+        />
+        <strong>Monitor gamer</strong>
+        <span>R$1.729,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://img.terabyteshop.com.br/produto/g/monitor-gamer-hq-curvo-27-pol-165hz-1ms-freesync-hdmi-display-port_82529.jpg"
+          alt="Monitor gamer"
+        />
+        <strong>Monitor gamer</strong>
+        <span>R$1.729,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://img.terabyteshop.com.br/produto/g/monitor-gamer-hq-curvo-27-pol-165hz-1ms-freesync-hdmi-display-port_82529.jpg"
+          alt="Monitor gamer"
+        />
+        <strong>Monitor gamer</strong>
+        <span>R$1.729,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://img.terabyteshop.com.br/produto/g/monitor-gamer-hq-curvo-27-pol-165hz-1ms-freesync-hdmi-display-port_82529.jpg"
+          alt="Monitor gamer"
+        />
+        <strong>Monitor gamer</strong>
+        <span>R$1.729,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
